5638e6e4d875497e62378cc8a4f0d816bdb66fe9
tsikonomova <tsikonom@gmail.com>
2015-03-24 23:37:44 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,185 @@
+var text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren" + "</br>" +
+	"Lorem ipsum dolor sit amet, consetetur sadipscing elitr," + "</br>" +
+    " sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat," + "</br>" +
+	" sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren"
+
+function fillWithTextLeft () {
+	$(".leftContainer p")[0].innerHTML = text;
+}
+
+function fillWithTextRight () {
+	$(".rightContainer p")[0].innerHTML = text;
+}
+
+function emptyText (position) {
+	if (position == "left")  {
+		$(".leftContainer p")[0].innerHTML = "";
+	}
+	else if (position == "right") {
+		$(".rightContainer p")[0].innerHTML = "";
+	}
+}
